80478a05996fb3d1063266c04100954f38749f2a
Cristian David Guzman Tamayo <cristian.guzman@lumiqualis.com>
2022-10-29 20:18:26 -0500

    add the Papa parse function

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from "react";
+import Papa from "papaparse";
+import completions from "Data/completions.csv";
 
 export const Home = () => {
+    let file = completions;
+      Papa.parse(file,{ 
+        download: true,
+        complete: function(result){
+            console.log(result.data)
+        }
+        
+    })
+
   return (
-    <div>Home</div>
-  )
-}
+    <>
+    </>
+  );
+};
